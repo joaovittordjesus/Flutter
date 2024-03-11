1898e36cd1748e2dcdfc7bf7ca031fb5eebb2dee
joaovittordjesus <joaovittordomingosdejesus@outlook.com>
2024-03-11 16:54:53 -0300

    Flutter exercicios

---
+++ ExerciciosLista/lista_um/lib/Exercicio3.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+      appBar: AppBar(
+        title: Text("Exercício 3"),
+      ),
+      body: Container(
+        child: Row(
+          children: [
+            Column(),
+          ],
+        ),
+      ),
+      ),
+    );
+  }
+
+}
