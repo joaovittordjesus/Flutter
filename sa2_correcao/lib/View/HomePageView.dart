b8cb5fc212108aec82dbb71b062e7eb09acb6c7a
joaovittordjesus <joaovittordomingosdejesus@outlook.com>
2024-04-15 16:50:06 -0300

    Correção sharedpref

---
+++ sa2_correcao/lib/View/HomePageView.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class PaginaHome extends StatefulWidget {
+  String email;
+  PaginaHome({required this.email});
+
+  @override
+  State<PaginaHome> createState() => _PaginaHomeState();
+}
+
+class _PaginaHomeState extends State<PaginaHome> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
